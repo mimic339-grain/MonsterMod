f8c8670e1ced82fa0721823c199dfc8bb5eaad87
mimic339-grain <zen.3393.00@gmail.com>
2025-07-09 19:04:13 +0900

    とりあえず完成形

--- src/main/java/net/mimic/monstermod/client/renderer/MimicRenderer.java
+++ src/main/java/net/mimic/monstermod/client/renderer/MimicRenderer.java
@@ -1,21 +1,21 @@
 package net.mimic.monstermod.client.renderer;
 
-import net.mimic.monstermod.client.model.MimicModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.mimic.monstermod.MonsterMod;
-import net.mimic.monstermod.entity.custom.MimicEntity;
+
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
+import net.mimic.monstermod.MonsterMod;
+import net.mimic.monstermod.client.model.MimicModel;
+import net.mimic.monstermod.entity.custom.MimicEntity;
+
 public class MimicRenderer extends GeoEntityRenderer<MimicEntity> {
-    public MimicRenderer(EntityRendererProvider.Context renderProvider) {
-        super(renderProvider, new MimicModel());
-        this.shadowRadius = 0.4f; // 影のサイズを調整
+    public MimicRenderer(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new MimicModel());
     }
 
     @Override
-    public ResourceLocation getTextureLocation(MimicEntity instance) {
-        // ここが重要：正しいテクスチャのパスを指定
-        return new ResourceLocation(MonsterMod.MOD_ID, "textures/entity/mimic/mimic.png");
+    public ResourceLocation getTextureLocation(MimicEntity animatable) {
+        return new ResourceLocation(MonsterMod.MOD_ID, "textures/entity/mimic.png");
     }
 }
